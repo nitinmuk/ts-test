14c361f9a2816727991466536bf318079bb71256
Nitin Mukesh <nitinmukes@gmail.com>
2022-02-06 10:43:14 +1030

    Refactored mapping logic for httpGet response to separate internal function

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -8,17 +8,25 @@ interface Failure {
 }
 type TResult = ArnieQuote | Failure;
 
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  const arnieQuoteResults = await Promise.all(urls.map(url => httpGet(url)));
-  return arnieQuoteResults.map(result => {
-    switch(result.status) {
+/**
+ * maps given httpGet function response object to TResult object
+ * @param httpGetResponse response of httpGet function
+ * @returns TResult
+ */
+const mapResponse = (httpGetResponse: {status: number, body: string}): TResult => {
+  switch(httpGetResponse.status) {
     case 200:
       return {
-        'Arnie Quote': JSON.parse(result.body).message
+        'Arnie Quote': JSON.parse(httpGetResponse.body).message
       }
     default:
       return {
-        FAILURE: JSON.parse(result.body).message
+        FAILURE: JSON.parse(httpGetResponse.body).message
       }
-  }});  
+  }
+}
+
+export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
+  const arnieQuoteResponses = await Promise.all(urls.map(url => httpGet(url)));
+  return arnieQuoteResponses.map(response => mapResponse(response));  
 };
